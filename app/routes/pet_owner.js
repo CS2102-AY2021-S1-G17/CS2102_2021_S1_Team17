004f478ac57402e63c838ba9630c6f31a0a89112
Yuan Jiayi <yuanjiayi2000@gmail.com>
2020-10-26 01:59:32 +0800

    Add subpages for pet pwner

--- app/routes/pet_owner.js
+++ app/routes/pet_owner.js
@@ -2,7 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) { 
-    res.render('petowner', { title: 'Petowner Page'});
+    res.render('pet_owner/po_profile', { title: 'Petowner Page'});
 });
 
+router.get('/po_profile',  function(req, res, next) {
+      res.render('pet_owner/po_profile', { title: 'Profile Page', user : req.user });
+}); 
+
+router.get('/pets',  function(req, res, next) {
+      res.render('pet_owner/po_pets_profile', { title: 'Pets Profile Page', user : req.user });
+}); 
+
+router.get('/history',  function(req, res, next) {
+      res.render('pet_owner/po_history', { title: 'History Transaction Page', user : req.user });
+}); 
 module.exports = router;
