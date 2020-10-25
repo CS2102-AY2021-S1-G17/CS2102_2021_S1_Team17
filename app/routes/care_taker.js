099c6c4c85efdf620cc4bf32cf37290deda528b6
Yiping <e0357438@u.nus.edu>
2020-10-25 19:11:49 +0800

    add subpages for caretaker

--- app/routes/care_taker.js
+++ app/routes/care_taker.js
@@ -2,15 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) { 
-    if (req.user) {
-      res.render('caretaker', { title: 'Caretaker Page'});
-    }
+    res.render('care_taker/ct_profile', { title: 'Caretaker Page'});
 });
 
 /* GET Profile page. */ 
 router.get('/profile',  function(req, res, next) {
-      res.render('profile', { title: 'Profile Page', user : req.user,
-      avatar: gravatar.url(req.user.email ,  {s: '100', r: 'x', d:
-      'retro'}, true) });
-  }); 
+      res.render('care_taker/ct_profile', { title: 'Profile Page', user : req.user });
+}); 
+
+router.get('/profile_edit',  function(req, res, next) {
+      res.render('care_taker/ct_profile_edit', { title: 'Edit Profile', user : req.user });
+}); 
+
+router.get('/availability',  function(req, res, next) {
+      res.render('care_taker/ct_availability', { title: 'Availability Page', user : req.user });
+}); 
+
 module.exports = router;
--- app/routes/pet_owner.js
+++ app/routes/pet_owner.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) { 
-    if (req.user) {
-      res.render('petowner', { title: 'Petowner Page'});
-    }
+    res.render('petowner', { title: 'Petowner Page'});
 });
 
 module.exports = router;
