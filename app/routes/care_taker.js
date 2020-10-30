a2b9262207d68f87efde65aa5c148da41d8d4cac
Yiping <e0357438@u.nus.edu>
2020-10-30 17:09:42 +0800

    update ct pages

--- app/routes/care_taker.js
+++ app/routes/care_taker.js
@@ -10,8 +10,8 @@ router.get('/profile',  function(req, res, next) {
       res.render('care_taker/ct_profile', { title: 'Profile Page', user : req.user });
 }); 
 
-router.get('/profile_edit',  function(req, res, next) {
-      res.render('care_taker/ct_profile_edit', { title: 'Edit Profile', user : req.user });
+router.get('/salary',  function(req, res, next) {
+      res.render('care_taker/ct_salary', { title: 'Salary', user : req.user });
 }); 
 
 router.get('/availability',  function(req, res, next) {
