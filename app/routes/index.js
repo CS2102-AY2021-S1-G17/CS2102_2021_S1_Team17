e099b6754642726aaad8278b46c2e6cc2c8f7361
Yuxuan1998 <hanyuxuan19@gmail.com>
2020-11-01 23:52:59 +0800

    bcrypt password

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var db = require('../db');
+const bcrypt = require("bcrypt");
+
 /* GET login page. */ 
 router.get('/', function(req, res, next) { 
   if (req.user) {
@@ -16,9 +18,10 @@ router.post("/", async function (req, res, next) {
       /*--------------------------Sign up PO------------------------------ */
       if (req.body.signup_po) {
         let {username, phoneno, password, location, card, petname, category, requirement} = req.body;
+        let hashedP = await bcrypt.hash(password, 10);
         try {
             await db.query("CALL register_pet_owner($1, $2, $3, $4, $5, $6, $7, $8);",
-                [phoneno, password, location, username, card, petname, requirement, category]);
+                [phoneno, hashedP, location, username, card, petname, requirement, category]);
             req.flash("success", "Sign up success.");
         } catch (e) {
             console.log('below');
@@ -31,12 +34,13 @@ router.post("/", async function (req, res, next) {
         }      
       } else if (req.body.signup_ct) {   /*-------Sign up CT -----------*/
         let {username, phoneno, password, location, ba, ft, category, price} = req.body;
+        let hashedP = await bcrypt.hash(password, 10);
         if (String(price)==="") {
           price = 1;
         };
         try {
             await db.query("CALL register_caretaker($1, $2, $3, $4, $5, $6, $7, $8);",
-                [phoneno, String(password), String(location), String(username), String(ba), Boolean(ft), category, price]);
+                [phoneno, hashedP, String(location), String(username), String(ba), Boolean(ft), category, price]);
             req.flash("success", "Sign up success.");
         } catch (e) {
             console.log(e);
@@ -49,14 +53,15 @@ router.post("/", async function (req, res, next) {
       } else if (req.body.signup_both) {   /*-------Sign up Both -----------*/
         let {username, phoneno, password, p_location, card, petname, p_category, requirement,
            c_location, ba, b_ft, c_category, b_price} = req.body;
+        let hashedP = await bcrypt.hash(password, 10);
         if (String(b_price)==="") {
           b_price = 1;
         };
         try {
             await db.query("CALL register_caretaker($1, $2, $3, $4, $5, $6, $7, $8);",
-                [phoneno, String(password), String(c_location), String(username), String(ba), Boolean(b_ft), c_category, b_price]);
+                [phoneno, hashedP, String(c_location), String(username), String(ba), Boolean(b_ft), c_category, b_price]);
             await db.query("CALL register_pet_owner($1, $2, $3, $4, $5, $6, $7, $8);",
-                [phoneno, password, p_location, username, card, petname, requirement, p_category]);
+                [phoneno, hashedP, p_location, username, card, petname, requirement, p_category]);
             req.flash("success", "Sign up success.");
         } catch (e) {
             console.log(e);
