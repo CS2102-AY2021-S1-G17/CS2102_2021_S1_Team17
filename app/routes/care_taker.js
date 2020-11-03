ea2588c1fbb836eb25de6faef62bdfbdc47d2802
Yuxuan1998 <hanyuxuan19@gmail.com>
2020-11-04 03:35:50 +0800

    old sql files

--- app/routes/care_taker.js
+++ app/routes/care_taker.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var db = require('../db');
 var router = express.Router();
 
-router.get('/', function(req, res, next) { 
-    res.render('care_taker/ct_profile', { title: 'Caretaker Page'});
+router.get('/', async(req, res)=> { 
+    try{
+      var data = await db.query("SELECT * FROM care_taker ct WHERE ct.phone=$1;",[req.user.phone]);
+      var data2 = await db.query("SELECT category_name FROM capable WHERE phone=$1;",[req.user.phone]);
+      var name = data.rows.name;
+      if (data.rows.is_full_time) {
+        var full_part = "Full Time Employee";
+      } else {
+        var full_part = "Part Time Employee";
+      }
+      var rate = data.rows.avg_rating;
+      res.render('care_taker/ct_profile', { title: 'Caretaker Page'});
+    } catch (err) {
+      throw err;
+    }
 });
 
 /* GET Profile page. */ 
