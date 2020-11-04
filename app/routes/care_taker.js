bf583325fd59199a53c1b06377e9b986a3a20417
Yuxuan1998 <hanyuxuan19@gmail.com>
2020-11-04 14:47:26 +0800

    ct route get pending

--- app/routes/care_taker.js
+++ app/routes/care_taker.js
@@ -6,6 +6,7 @@ router.get('/', async(req, res)=> {
     try{
       var data = await db.query("SELECT * FROM care_taker ct WHERE ct.phone=$1;",[req.user.phone]);
       var data2 = await db.query("SELECT category_name FROM capable WHERE phone=$1;",[req.user.phone]);
+      var data3 = await db.query("SELECT * FROM ct_view_pending_bids($1);",[req.user.phone]);
       var name = data.rows.name;
       if (data.rows.is_full_time) {
         var full_part = "Full Time Employee";
@@ -13,7 +14,9 @@ router.get('/', async(req, res)=> {
         var full_part = "Part Time Employee";
       }
       var rate = data.rows.avg_rating;
-      
+      var cat_list = data2.rows;
+      var pending_bids = data3.rows; //contains [petowner, po_phone, pet_name, start_date ,end_date, total_cost, transfer_method, payment_method]
+      console.log(pending_bids);
       res.render('care_taker/ct_profile', { title: 'Caretaker Page'});
     } catch (err) {
       throw err;
