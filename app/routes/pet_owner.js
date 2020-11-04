3b72f2f918db88028831235bb701da828a0c0d94
yongyan <cs623329235@gmail.com>
2020-11-04 19:14:15 +0800

    get po's name

--- app/routes/pet_owner.js
+++ app/routes/pet_owner.js
@@ -16,8 +16,10 @@ router.get('/', async(req, res, next)=> {
 router.get('/po_profile',  async(req, res, next)=> { 
   try{
     var po_info = await db.query("SELECT * FROM pet_owner WHERE phone=$1;",[req.user.phone]);
+    var name = po_info.rows[0].name;
     var bids_info = await db.query("SELECT * FROM bids WHERE po_phone=$1;",[req.user.phone]);
-    res.render('pet_owner/po_profile', { title: req.user.name+"'s Page", user : req.user });
+    console.log(bids_info);
+    res.render('pet_owner/po_profile', { title: name+"'s Page", user : req.user });
   } catch (err) {
     throw err;
   }
@@ -25,9 +27,11 @@ router.get('/po_profile',  async(req, res, next)=> {
 
 router.get('/pets',  async(req, res, next)=> {
   try {
-    var pets_info = await db.query("SELECT po_view_pets($1);",[req.user.phone]);
+    var po_info = await db.query("SELECT * FROM pet_owner WHERE phone=$1;",[req.user.phone]);
+    var name = po_info.rows[0].name;
+    var pets_info = await db.query("SELECT * FROM po_view_pets($1);",[req.user.phone]);
     console.log(pets_info);
-    res.render('pet_owner/po_pets_profile', { title: req.user.name+"'s Page", user : req.user });
+    res.render('pet_owner/po_pets_profile', { title: name+"'s Pets", user : req.user });
   } catch (err) {
     throw err;
   }  
@@ -35,15 +39,23 @@ router.get('/pets',  async(req, res, next)=> {
 
 router.get('/history',  async(req, res, next)=> {
   try {
-    var trans_info = await db.query("SELECT po_view_past_trans($1);",[req.user.phone]);
+    var po_info = await db.query("SELECT * FROM pet_owner WHERE phone=$1;",[req.user.phone]);
+    var name = po_info.rows[0].name;
+    var trans_info = await db.query("SELECT * FROM po_view_past_trans($1);",[req.user.phone]);
     console.log(trans_info);
-    res.render('pet_owner/po_history', { title: req.user.name+"'s History Transaction Page", user : req.user });
+    res.render('pet_owner/po_history', { title: name+"'s History Transaction Page", user : req.user });
   } catch (err) {
     throw err;
   }
 }); 
 
 router.get('/bid',  async(req, res, next)=> {
-      res.render('pet_owner/po_bid', { title: req.user.name+"'s Bid Page", user : req.user });
+  try {
+    var po_info = await db.query("SELECT * FROM pet_owner WHERE phone=$1;",[req.user.phone]);
+    var name = po_info.rows[0].name;
+    res.render('pet_owner/po_bid', { title: name+"'s Bid Page", user : req.user });
+  } catch (err) {
+    throw err;
+  }
 }); 
 module.exports = router;
