c6e5b640e5a714f15d73ebd0c41486e796be8ed2
Yuxuan1998 <hanyuxuan19@gmail.com>
2020-11-06 22:14:56 +0800

    po pay

--- routes/pet_owner.js
+++ routes/pet_owner.js
@@ -51,6 +51,31 @@ router.post('/update_location', async(req, res)=> {
 
 router.post('/pay', async(req, res)=> {
   try{
+    let {po_phone, ct_phone, pet_name, start_date, end_date, pay} = req.body;
+    
+    function convert(str) {
+      var mnths = {
+          Jan: "01",
+          Feb: "02",
+          Mar: "03",
+          Apr: "04",
+          May: "05",
+          Jun: "06",
+          Jul: "07",
+          Aug: "08",
+          Sep: "09",
+          Oct: "10",
+          Nov: "11",
+          Dec: "12"
+        },
+        date = str.split(" ");
+      return [date[3], mnths[date[1]], date[2]].join("-");
+    }
+    var start = convert(start_date);
+    var end = convert(end_date);
+
+    await db.query("CALL change_bid_status($1, $2, $3,$4, $5,'Success')",[parseInt(po_phone), parseInt(ct_phone), pet_name, start, end]);
+
     console.log(req.body);
   } catch (err) {
     throw err;
