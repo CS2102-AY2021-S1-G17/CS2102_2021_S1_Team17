3a247a7a7924ae0d51c5f4c8048d247a90b01eae
Yuxuan1998 <hanyuxuan19@gmail.com>
2020-11-08 00:48:55 +0800

    po details button

--- routes/pet_owner.js
+++ routes/pet_owner.js
@@ -218,4 +218,11 @@ router.post('/search',  async(req, res)=> {
         successFlash: req.flash("success"), errorFlash: req.flash("error")});
   }
 }); 
+
+router.post('/view_details', async(req, res) =>{
+  let {details} = req.body;
+  var data3 = await db.query("SELECT * FROM ct_view_past_trans($1);",[details]);
+  res.render('pet_owner/ct_history', { title: 'History Page', history: data3.rows, successFlash: req.flash("success"),
+  errorFlash: req.flash("error")});
+})
 module.exports = router;
